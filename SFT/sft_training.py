f70c5c64c959ea2d934d57b751fed951f3895d5c
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2025-06-19 22:44:44 -0600

    Add learning rate SFT trainer

--- SFT/sft_training.py
+++ SFT/sft_training.py
@@ -21,6 +21,7 @@ training_args = SFTConfig(
     per_device_eval_batch_size=4,
     gradient_accumulation_steps=1,
     lr_scheduler_type="cosine",
+    learning_rate=1.0e-4,
     logging_steps=1,
     max_length=32768,
     output_dir="tmp",
